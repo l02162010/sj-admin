f290d373a9d6c4206fb4b9e64b0d31d37c377184
Austin <augustine.lin@mic.com.tw>
2019-10-05 19:27:36 +0800

    use youtube api

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const upload = multer({ dest: __dirname +'/new/'});
 const type = upload.single('recfile');
 const videoController = require('../controllers/videoController')
 const imageController = require('../controllers/imageController')
+const googleAPIKey = require('../config/google_api.json')
+const {google} = require('googleapis');
+const opn = require('open');
 
 
 const title = 'SJ-02 ADMIN'
@@ -18,8 +21,65 @@ router.get('/logout', async (req, res, next) => {
   res.clearCookie('session');
   return res.redirect('/login')
 });
-
+let googleToken = ''
 router.get('/video', async (req, res) => {
+  console.log(req.query)
+  const oauth2Client = new google.auth.OAuth2(
+    googleAPIKey.web.client_id,
+    'lJhnRCT7gZbEyAGwHiG6Bd1-',
+    'http://localhost:3002/video'
+  );
+  // generate a url that asks permissions for Blogger and Google Calendar scopes
+  const scopes = [
+    'https://www.googleapis.com/auth/youtube'
+  ];
+  let youtubeList = []
+  if(!req.query.code){
+    const url = oauth2Client.generateAuthUrl({
+      // 'online' (default) or 'offline' (gets refresh_token)
+      access_type: 'offline',
+      // If you only need one scope you can pass it as a string
+      scope: scopes
+    });
+    opn(url, {wait: false}).then(cp => cp.unref());
+  }else if(!googleToken){
+    const {tokens} = await oauth2Client.getToken(req.query.code)
+    googleToken = tokens
+    oauth2Client.on('tokens',async(tokens) => {
+      if (tokens.refresh_token) {
+        // store the refresh_token in my database!
+        // 記得存啊
+        oauth2Client.setCredentials({
+          refresh_token: tokens.refresh_token
+        });
+      }
+    });
+  }
+  if(googleToken){
+    oauth2Client.setCredentials(googleToken);
+    const youtube = google.youtube({
+      version: 'v3',
+      auth: oauth2Client
+    });
+    const params = {
+      "part": "snippet",
+      "forMine": true,
+      "type": "video",
+      "maxResults": 50
+    };
+    // get the youtube details
+    const res = await youtube.search.list(params)
+    youtubeList = res.data.items.map(d => {
+      return {
+        id: d.id.videoId,
+        title: d.snippet.title,
+        imgUrl_small: d.snippet.thumbnails.default.url,
+        imgUrl_big: d.snippet.thumbnails.high.url,
+      }
+    })
+  }
+ 
+
   let featchRes = await videoController.getVideos()
   const list = featchRes.map(f => {
     return {
@@ -31,7 +91,7 @@ router.get('/video', async (req, res) => {
       imgUrl: `https://${req.hostname}/image/${f.imgId}`
     }
   })
-  res.render('video', { title, pageTitle: '影片設定', list });
+  res.render('video', { title, pageTitle: '影片設定', list, youtubeList });
 });
 
 router.post('/video', type, async (req, res) => {
@@ -87,5 +147,26 @@ router.get('/image/:imgId',async (req, res) => {
     res.statusCode(404);
   }
 });
+
+let getAuth = () => {
+  const oauth2Client = new google.auth.OAuth2(
+    googleAPIKey.web.client_id,
+    'lJhnRCT7gZbEyAGwHiG6Bd1-',
+    'http://localhost:3002/video'
+  );
+  
+  // generate a url that asks permissions for Blogger and Google Calendar scopes
+  const scopes = [
+    'https://www.googleapis.com/auth/youtube'
+  ];
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+  
+    // If you only need one scope you can pass it as a string
+    scope: scopes
+  });
+  opn(url, {wait: false}).then(cp => cp.unref());
+}
 
 module.exports = router;
